66d68c7998fe0a15b85465d5f9604619d0b924e0
bleowy <bleowy1@gmail.com>
2015-04-04 18:57:24 +0200

    Basics
    Some basics...

---
+++ index.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="style/style.css"/>
+<html>
+    <head>
+        <div class="head">
+            <h5><a href="/">Role Playing Engine</a></h5>
+        </div>
+    </head>
+    <body>
+        <div class="container">
+            <div id="left_menu">
+            Role Playing Engine is Open Source project based on PHP game engine.
+            Created by still noobish programist called Bleo.
+            Feel free to check code on github which link is placed at the bottom of site.
+            </div>
+            <div id="right_menu">
+                <form action="/login.php" method="post">
+                Login:
+                <input type="text" name="login">
+				Password:
+				<input type="password" name="password" >
+				<input type="submit" value="Login">
+                </form>
+            </div>
+        </div>
+    </body>
+    <div id="footer">
+        Engine Created by <a href="http://www.github.com/bleowy">Bleowy</a>.Check code at <a href="http://www.github.com">Github</a>
+    </div>
+</html>
---
+++ lang/pl.php
@@ -0,0 +1,7 @@
+class language
+{
+    public function text()
+    {
+    $this -> main_page_text = 'Role Playing Engine to projekt open-souce.Obecnie jest pisany przez mało doświadczonego programistę.';
+    }
+}
